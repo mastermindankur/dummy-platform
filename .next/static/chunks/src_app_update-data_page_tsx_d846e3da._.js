576a4eb7d55fc26ae7c64bf6b5346924cc6db971
ankur khera <mastermindankur@gmail.com>
2025-09-15 18:49:55 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@r

--- .next/static/chunks/src_app_update-data_page_tsx_d846e3da._.js
+++ .next/static/chunks/src_app_update-data_page_tsx_d846e3da._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/src_3c5272ab._.js",
-  "static/chunks/node_modules_1b9022ca._.js"
+  "static/chunks/node_modules_32367712._.js"
 ],
     source: "dynamic"
 });
