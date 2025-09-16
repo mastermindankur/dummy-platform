f4fd349c5a8b6695515856a70c9052f4b03030ef
ankur khera <mastermindankur@gmail.com>
2025-09-16 15:14:11 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 're

--- .next/static/chunks/src_app_update-data_page_tsx_d846e3da._.js
+++ .next/static/chunks/src_app_update-data_page_tsx_d846e3da._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_b08a8674._.js",
-  "static/chunks/node_modules_cde4a48a._.js"
+  "static/chunks/src_09e28486._.js",
+  "static/chunks/node_modules_eac70f7e._.js"
 ],
     source: "dynamic"
 });
